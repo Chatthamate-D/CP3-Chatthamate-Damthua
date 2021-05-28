3215bdbae279e307c1f3db5e7c565a1fd9c96872
Chatthamate-D <chatthamate.d@hotmail.com>
2021-05-29 01:52:20 +0700

    Create Exercise4_Chatthamate_D.py
    The first Exercise with Github

---
+++ Exercise4_Chatthamate_D.py
@@ -0,0 +1,14 @@
+'''
+Your Score
+'''
+
+foueng = 80 # Foundation English
+genbus = 60.50 # General Business'''
+introcom = 76.89 #Intraduction to computer systems
+compro = 90.99 # Computer Programming
+
+print("---Your Score---")
+print("Foundation English :",foueng)
+print("General Business :", genbus)
+print("Introduction to Computer Systems :", introcom)
+print("Computer Programming :", compro)
